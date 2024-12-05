27951f4a6353dbbc0954bb3a7402f8c6021310d7
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2024-12-05 13:41:36 +0700

    update db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 // Membuat koneksi pool ke Cloud SQL menggunakan IP Publik
 const pool = mysql.createPool({
   host: '34.128.78.215',  // IP Publik Cloud SQL (ganti dengan IP yang sesuai)
-  user: 'test-cloudsql',   // Username untuk Cloud SQL
+  user: 'root',   // Username untuk Cloud SQL
   password: '',  // Password untuk Cloud SQL (ganti dengan password yang benar)
   database: 'auth_system'  // Nama database Anda
 });
